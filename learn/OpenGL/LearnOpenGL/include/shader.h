1f090fad927f8d4406fe0ea64c5d847a788f9181
Lemon <62458905+lemonsama123@users.noreply.github.com>
2023-04-20 23:51:57 +0800

    睡觉前代码备份

--- learn/OpenGL/LearnOpenGL/include/shader.h
+++ learn/OpenGL/LearnOpenGL/include/shader.h
@@ -10,6 +10,9 @@
 #include <fstream>
 #include <sstream>
 #include <iostream>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
 
 class Shader {
 public:
@@ -112,6 +115,10 @@ public:
 
     void setFloat(const std::string &name, float value1, float value2, float value3, float, float value4) const {
         glUniform4f(glGetUniformLocation(ID, name.c_str()), value1, value2, value3, value4);
+    }
+
+    void setMatrix4(const std::string &name, glm::mat4 value) const {
+        glUniformMatrix4fv(glGetUniformLocation(ID, name.c_str()), 1, GL_FALSE, glm::value_ptr((value)));
     }
 
 private:
