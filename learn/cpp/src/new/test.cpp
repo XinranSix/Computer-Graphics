58f8bc67c85f897d9038c04eaa52df87b2a87a6e
Jie Yao <jieyao.lxr@foxmail.com>
2023-05-15 19:37:03 +0800

    asd

--- learn/cpp/src/new/test.cpp
+++ learn/cpp/src/new/test.cpp
@@ -2,12 +2,9 @@
 // Created by yaojie on 2023/5/12.
 //
 
-#include <boost/array.hpp>
-#include <iostream>
+#include <fmt/core.h>
 
-using namespace std;
 int main() {
-    boost::array<int, 4> arr = {{1, 2, 3, 4}};
-    cout << "hi" << arr[0];
+    fmt::print("helloworl{}", 123);
     return 0;
 }
