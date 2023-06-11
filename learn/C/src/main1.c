fc9606625c8b54b8af2c9e7357e4e34559cb1580
Yao Jie <jieyao.lxr@foxmail.com>
2023-06-11 13:36:43 +0800

    睡觉前代码备份

--- learn/C/src/main1.c
+++ learn/C/src/main1.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 
 int main(int argc, char *argv[]) {
-    printf("Hello Main1");
+    int i = 1;
+    int sum = 0;
+JOOP:
+    sum += i;
+    i++;
+    if (i <= 100) {
+        goto JOOP;
+    }
+    printf("1 + 2 + 3 + ... + 100 = %d\n", sum);
     return 0;
 }
