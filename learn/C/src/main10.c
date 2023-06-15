871112f1eeb57d635cc13837f946f153d697f7e5
Yao Jie <jieyao.lxr@foxmail.com>
2023-06-15 21:49:51 +0800

    更新代码

--- learn/C/src/main10.c
+++ learn/C/src/main10.c
@@ -1,13 +1,3 @@
 #include <stdio.h>
 
-#define AAA
-
-int main(int argc, char *argv[]) {
-
-#ifdef AAA
-    printf("hello kitty!!\n");
-#else
-    printf("hello 千锋edu\n");
-#endif
-    return 0;
-}
+int main(int argc, char *argv[]) {}
