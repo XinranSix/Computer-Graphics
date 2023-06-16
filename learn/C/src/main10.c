91249df9858b8bb3f7c1597a5a1022f01bb37d8e
Yao Jie <jieyao.lxr@foxmail.com>
2023-06-16 16:51:15 +0800

    更新代码

--- learn/C/src/main10.c
+++ learn/C/src/main10.c
@@ -1,15 +1,11 @@
 #include <stdio.h>
 
-void fun(int (*p)[5], int x, int y) { p[0][1] = 101; }
-
-int main() {
-    int a[3][5] = {0};
-    fun(a, 3, 5);
-    for (int i = 0; i < 3; i++) {
-        for (int j = 0; j < 5; j++) {
-            printf("%d ", a[i][j]);
-        }
-        printf("\n");
+int main(int argc, char *argv[]) {
+    int i;
+    printf("argc=%d\n", argc);
+    for (i = 0; i < argc; i++) {
+        printf("argv[%d]=%s\n", i, argv[i]);
     }
+
     return 0;
 }
