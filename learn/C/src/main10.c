7b7eea942269c57479ec0b75d433c02228002a74
Yao Jie <jieyao.lxr@foxmail.com>
2023-06-16 14:01:22 +0800

    更新代码

--- learn/C/src/main10.c
+++ learn/C/src/main10.c
@@ -1,19 +1,15 @@
 #include <stdio.h>
 
 int main(int argc, char *argv[]) {
-    char *a;
-    short *b;
-    int *c;
-    long *d;
-    float *e;
-    double *f;
 
-    printf("sizeof(a) = %lld\n", sizeof(a));
-    printf("sizeof(b) = %lld\n", sizeof(b));
-    printf("sizeof(c) = %lld\n", sizeof(c));
-    printf("sizeof(d) = %lld\n", sizeof(d));
-    printf("sizeof(e) = %lld\n", sizeof(e));
-    printf("sizeof(f) = %lld\n", sizeof(f));
+    int a = 100;
+    int *p = &a;
+    int **q = &p;
+
+    printf("a = %d %d %d\n", a, *p, **q);
+    printf("&a = %p %p %p\n", &a, p, *q);
+    printf("&p = %p %p\n", &p, q);
+    printf("&q = %p\n", &q);
 
     return 0;
 }
