343c58076ddd2fae171ee684b2aa8bd353824121
Yao Jie <jieyao.lxr@foxmail.com>
2023-06-16 18:50:55 +0800

    更新代码

--- learn/C/src/main10.c
+++ learn/C/src/main10.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(int argc, char *argv[]) {
-    int i;
-    printf("argc=%d\n", argc);
-    for (i = 0; i < argc; i++) {
-        printf("argv[%d]=%s\n", i, argv[i]);
-    }
+void fun() { char *p = (char *)malloc(100); }
 
+int main() {
+    // 每调用一次fun泄露100个字节
+    fun();
+    fun();
     return 0;
 }
