9a1d1f8d1b2734a33f17b940ab0304ad0dbf8a88
Yao Jie <jieyao.lxr@foxmail.com>
2023-06-18 12:28:47 +0800

    update

---
+++ learn/cpp/src/test/test1.cpp
@@ -0,0 +1,13 @@
+/**
+ * @file    :   test1.cpp
+ * @date    :   2023/06/18 12:24:18
+ * @author  :   yaojie
+ * @version :   1.0
+ */
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello World" << std::endl;
+    return 0;
+}
