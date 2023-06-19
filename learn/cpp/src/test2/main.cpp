af658b2a4977ebf8862b56e395cc65b0c260e063
lxr <jieyao.lxr@foxmail.com>
2023-06-19 17:14:45 +0800

    123

---
+++ learn/cpp/src/test2/main.cpp
@@ -0,0 +1,13 @@
+/**
+ * @file    :   main.cpp
+ * @date    :   2023/06/19 16:11:29
+ * @author  :   yaojie
+ * @version :   1.0
+ */
+
+#include <iostream>
+
+int main() {
+    printf("hello world\n");
+    return 0;
+}
