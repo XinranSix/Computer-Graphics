dd7caad28f965a2e7aea4bae15c11bc5a980c77d
lxr <jieyao.lxr@foxmail.com>
2023-06-20 12:16:57 +0800

    update

--- learn/cpp/src/test3/main.cpp
+++ learn/cpp/src/test3/main.cpp
@@ -1,13 +1,40 @@
 #include <iostream>
 #include <memory>
+#include <cstring>
 
 using namespace std;
 
 class Person {
-    Person(){};
+public:
+    Person() {}
+    Person(int age1, char *name1) {
+        age = age1;
+        name = new char[strlen(name1) + 1];
+        strcpy(name, name1);
+    }
+    ~Person() { delete[] name; }
+
+public:
+    Person &operator=(Person &p) {
+        this->age = p.age;
+        name = new char[strlen(p.name) + 1];
+        strcpy(name, p.name);
+        return *this;
+    }
+
+public:
+    int age;
+    char *name;
 };
 
+void test01() {
+    Person p1{10, "bob"};
+    Person p2;
+    p2 = p1;
+    cout << p2.age << ' ' << p2.name << endl;
+}
+
 int main() {
-    unique_ptr<Person> p;
+    test01();
     return 0;
 }
