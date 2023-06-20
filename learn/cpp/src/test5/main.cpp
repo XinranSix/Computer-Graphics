53425f47629c34dfc98a96b2d097f8208f984f92
lxr <jieyao.lxr@foxmail.com>
2023-06-20 23:47:15 +0800

    update

--- learn/cpp/src/test5/main.cpp
+++ learn/cpp/src/test5/main.cpp
@@ -1,21 +1,32 @@
+/**
+ * @file    :   main.cpp
+ * @date    :   2023/06/20 17:50:53
+ * @author  :   yaojie
+ * @version :   1.0
+ * 模板测试
+ */
+
 #include <iostream>
-#include <string>
 
 using namespace std;
 
-class Calc {
-public:
-    int calc(int a, int b, string cmd) {
-        if (cmd == "+") {
-            return a + b;
+template<typename T>
+void arr_sort(T arr[], int n) {
+    for (int i = 0; i < n - 1; ++i) {
+        int idx = i;
+        for (int j = i + 1; j < n; ++j) {
+            if (min > arr[j]) {
+                idx = j;
+            }
         }
-        return 0;
+        if (i != idx) {
+            T t = arr[idx];
+            arr[idx] = arr[i];
+            arr[i] = t;
+        }
     }
-};
+}
 
 void test01() {}
 
-int main() {
-    test01();
-    return 0;
-}
+int main() { return 0; }
