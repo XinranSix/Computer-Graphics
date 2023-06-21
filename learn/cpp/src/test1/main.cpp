9cd7f41145db0a3a83a404fc70c301a433ab2add
lxr <jieyao.lxr@foxmail.com>
2023-06-21 15:37:40 +0800

    update

--- learn/cpp/src/test1/main.cpp
+++ learn/cpp/src/test1/main.cpp
@@ -1,19 +1,14 @@
+#include <boost/version.hpp>
+#include <boost/config.hpp>
+
 #include <iostream>
-#include <gtest/gtest.h>
 
-int add(int a, int b) { return a + b; }
+using namespace std;
 
-TEST(testCase1, should_3_when_given_2_and_1) { EXPECT_EQ(add(2, 1), 3); }
-
-TEST(testCase2, should_3_when_given_2_and_1) { EXPECT_EQ(add(2, 1), 3); }
-
-TEST(testCase3, should_3_when_given_2_and_1) { EXPECT_EQ(add(2, 1), 3); }
-
-TEST(testCase4, should_3_when_given_2_and_1) { EXPECT_EQ(add(2, 1), 3); }
-
-TEST(testCase5, should_3_when_given_2_and_1) { EXPECT_EQ(add(2, 1), 3); }
-
-int main(int argc, char *argv[]) {
-    testing::InitGoogleTest(&argc, argv);
-    return RUN_ALL_TESTS();
+int main() {
+    cout << BOOST_VERSION << endl;
+    cout << BOOST_LIB_VERSION << endl;
+    cout << BOOST_STDLIB << endl;
+    cout << BOOST_PLATFORM << endl;
+    cout << BOOST_COMPILER << endl;
 }
