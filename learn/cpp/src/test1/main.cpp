e568d3cc4b9d41f7e1ddf7ce2e760a72f158e170
lxr <jieyao.lxr@foxmail.com>
2023-06-21 19:35:03 +0800

    update

--- learn/cpp/src/test1/main.cpp
+++ learn/cpp/src/test1/main.cpp
@@ -1,14 +1,13 @@
-#include <boost/asio.hpp>
 #include <iostream>
+#include <exception>
 
-void handler(const boost::system::error_code &ec) {
-    std::cout << "5 s." << std::endl;
-}
+using namespace std;
+
+void fun() {}
+
+void test01() {}
 
 int main() {
-    boost::asio::io_service io_service;
-    boost::asio::deadline_timer timer(io_service,
-                                      boost::posix_time::seconds(5));
-    timer.async_wait(handler);
-    io_service.run();
+    test01();
+    return 0;
 }
