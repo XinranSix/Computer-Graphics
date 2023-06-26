f2da3e52f4525911f11bc5ebaae6a03db0f410e1
lxr <jieyao.lxr@foxmail.com>
2023-06-26 16:28:20 +0800

    update

---
+++ learn/cpp/include/test.h
@@ -0,0 +1,12 @@
+/**
+ * @FileName       : test.h
+ * @Author         : yaojie
+ * @Date           : 2023/6/25
+ */
+
+#ifndef CPP_TEST_H
+#define CPP_TEST_H
+
+void print_hello_world();
+
+#endif // CPP_TEST_H
---
+++ learn/cpp/src/test2/test.cpp
@@ -0,0 +1,12 @@
+/**
+ * @FileName       : test.cpp
+ * @Author         : yaojie
+ * @Date           : 2023/6/25
+ */
+
+#include "test.h"
+#include <iostream>
+
+using namespace std;
+
+void print_hello_world() { cout << "hello world" << endl; }
